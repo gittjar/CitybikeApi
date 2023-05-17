b7c0c3c1863ac41caca3f58bc56ed5a695975df7
gittjar <jarno.keranen@gmail.com>
2023-05-17 23:17:14 +0300

    model created

--- CitybikeApi/Models/Biketrips2021-5.cs
+++ CitybikeApi/Models/Biketrips2021-5.cs
@@ -5,7 +5,13 @@ namespace CitybikeApi.Models
 	{
 
 		public DateTime Departure { get; set; }
-		public DateTime
+		public DateTime Return { get; set; }
+		public int Departure_station_id { get; set; }
+		public string? Departure_station_name { get; set; }
+		public int Return_station_id { get; set; }
+		public string? Return_station_name { get; set; }
+		public int Covered_distance_m { get; set; } // change int to SQL!
+		public int Duration_sec { get; set; }
 
 	}
 }
