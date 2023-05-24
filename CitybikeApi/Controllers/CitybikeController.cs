cbb9aa36a403d3ab3c757342d0b6590e3fccfe47
gittjar <jarno.keranen@gmail.com>
2023-05-24 10:20:54 +0300

    added serverside pagination for stations

--- CitybikeApi/Controllers/CitybikeController.cs
+++ CitybikeApi/Controllers/CitybikeController.cs
@@ -22,13 +22,44 @@ namespace CitybikeApi.Controllers
 
         // GET: api/Stations
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Station>>> GetStations()
+        public async Task<ActionResult<IEnumerable<Station>>> GetStations(int pageNumber = 1, int pageSize = 30)
         {
             if (_context.Station == null)
             {
                 return NotFound();
             }
-            return await _context.Station.ToListAsync();
+
+            var skip = (pageNumber - 1) * pageSize;
+            var take = pageSize;
+
+
+            var totalStations = await _context.Station.CountAsync();
+
+            var citybikestations = await _context.Station
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.Osoite)
+                .Distinct() // take only 1 item
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+
+            var totalPages = totalStations / 30; // 1 sivu = 30 asemaa
+
+            var result = new
+            {
+                TotalItems = totalStations,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalPages = totalPages,
+                Data = citybikestations,
+
+            };
+
+            return Ok(result);
+
+
+
+            // return await _context.Station.ToListAsync();
         }
 
           private bool StationExists(int id)
