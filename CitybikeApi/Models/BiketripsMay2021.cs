9fb8664397563f119654a45cf505a0e4c8dbc5c9
Jarno K <112698736+gittjar@users.noreply.github.com>
2025-11-10 19:08:34 +0200

    Add Id property to BiketripsMay2021 model

--- CitybikeApi/Models/BiketripsMay2021.cs
+++ CitybikeApi/Models/BiketripsMay2021.cs
@@ -3,18 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CitybikeApi.Models
 {
-	// Keyless model for API
     [Keyless]
     public class BiketripsMay2021
 	{
-		public DateTime Departure { get; set; }
-		public DateTime Return { get; set; }
-		public int Departure_station_id { get; set; }
-		public string? Departure_station_name { get; set; }
-		public int Return_station_id { get; set; }
-		public string? Return_station_name { get; set; }
-		public decimal Covered_distance_m { get; set; }
-		public int Duration_sec { get; set; }
+        public int Id { get; set; } 
+        public DateTime Departure { get; set; }
+        public DateTime Return { get; set; }
+        public int Departure_station_id { get; set; }
+        public string? Departure_station_name { get; set; }
+        public int Return_station_id { get; set; }
+        public string? Return_station_name { get; set; }
+        public decimal Covered_distance_m { get; set; }
+        public int Duration_sec { get; set; }
 
 	}
 }
